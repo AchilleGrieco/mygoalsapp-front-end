2417b161ef7c7bdf44771102e6ee60f8a435b0ca
Achille Grieco <grieco.achille01@gmail.com>
2024-12-28 22:33:07 +0100

    commit

--- lib/utility/config.dart
+++ lib/utility/config.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  static String get apiUrl => dotenv.env['API_URL'] ?? '192.168.1.6:8080';
+  static String get apiUrl => dotenv.env['API_URL']!;
 }
